3475e3c75c4f2576a29994c18b7b96433b2471cb
AninaRJ <ranjani.shivashankar@gmail.com>
2023-11-28 16:50:45 +0530

    Revert changes

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BooleanEditor } from '@contentful/field-editor-boolean';
 import { DisplayText } from '@contentful/f36-components';
-import { SingleLineEditor } from '@contentful/field-editor-single-line';
 
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 
@@ -11,11 +10,10 @@ const Field = () => {
   if(sdk.field.id === 'longFormBody'){
     return <>
       <DisplayText>Are sections anchored?</DisplayText>
-      <BooleanEditor 
+      <BooleanEditor  
         field={sdk.field}>
       </BooleanEditor>
 
-      <SingleLineEditor></SingleLineEditor>
     </>;
   }
   else{
