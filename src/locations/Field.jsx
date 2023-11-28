ec331f16afb31f102f439476955b40e6c94494f6
AninaRJ <ranjani.shivashankar@gmail.com>
2023-11-28 17:03:38 +0530

    Change everything to rich field

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { BooleanEditor } from '@contentful/field-editor-boolean';
 import { DisplayText } from '@contentful/f36-components';
-import { SingleLineEditor } from '@contentful/field-editor-single-line';
 
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+import { RichTextEditor } from '@contentful/field-editor-rich-text';
 
 const Field = () => {
   const sdk = useSDK();
@@ -11,10 +10,7 @@ const Field = () => {
   if(sdk.field.id === 'longFormBody'){
     return <>
       <DisplayText>Are sections anchored?</DisplayText>
-      <BooleanEditor >
-      </BooleanEditor>
-
-      <SingleLineEditor field={sdk.field} locales={sdk.locales}></SingleLineEditor>
+      <RichTextEditor field={sdk.field}></RichTextEditor>
     </>;
   }
   else{
