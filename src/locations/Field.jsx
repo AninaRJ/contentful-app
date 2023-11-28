e2ce3ade89f9de240df4df5f71d1ec1352d6e1fc
AninaRJ <ranjani.shivashankar@gmail.com>
2023-11-28 13:18:06 +0530

    Changed header to navbar

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Header, Textarea } from '@contentful/f36-components';
+import { Navbar, Textarea } from '@contentful/f36-components';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 
 const Field = () => {
   const sdk = useSDK();
  
-  return <><Header></Header><Textarea></Textarea></>;
+  return <><Navbar></Navbar><Textarea></Textarea></>;
 };
 
 export default Field;
