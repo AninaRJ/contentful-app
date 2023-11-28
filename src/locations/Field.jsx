278f89d832f27a33d5c0f9275b9565dd133bacd7
AninaRJ <ranjani.shivashankar@gmail.com>
2023-11-28 17:54:30 +0530

    Changes for card

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { EntryCard } from '@contentful/f36-components';
+import { Card } from '@contentful/f36-components';
 
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 import { RichTextEditor } from '@contentful/field-editor-rich-text';
 
 const Field = () => {
   const sdk = useSDK();
+  const [show, setShow] = React.useState(false);
 
-    return (<>
-      <RichTextEditor sdk={sdk} isInitiallyDisabled={false} minHeight={300}>
-        <EntryCard contentType="inputField"></EntryCard>
-      </RichTextEditor>
-    </>);
+  return (<>
+    <RichTextEditor sdk={sdk} isInitiallyDisabled={false} minHeight={900}>
+      <Card onClick={() => setShow(!show)}>
+        Click on this card
+        <br />
+        {show && (
+          <span role="img" aria-label="sparkles">
+          </span>
+        )}
+      </Card>
+    </RichTextEditor>
+  </>);
 };
 
 export default Field;
