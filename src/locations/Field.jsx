41e2aa45a63f1d95235a9d4bbcaffb10417d874d
AninaRJ <ranjani.shivashankar@gmail.com>
2023-11-28 13:19:46 +0530

    remove sdk

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -3,7 +3,6 @@ import { Navbar, Textarea } from '@contentful/f36-components';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 
 const Field = () => {
-  const sdk = useSDK();
  
   return <><Navbar></Navbar><Textarea></Textarea></>;
 };
