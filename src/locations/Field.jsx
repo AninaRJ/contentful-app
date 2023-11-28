07fd5691df0b69e0659f32e6f5256209cff87487
AninaRJ <ranjani.shivashankar@gmail.com>
2023-11-28 18:20:36 +0530

    Update header action

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Card } from '@contentful/f36-components';
-import { DisplayText } from '@contentful/f36-components';
 import { TextInput } from '@contentful/f36-components';
 
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
@@ -9,18 +8,20 @@ import { RichTextEditor } from '@contentful/field-editor-rich-text';
 const Field = () => {
   const sdk = useSDK();
   const [show, setShow] = React.useState(false);
+  const [sectionHeader, setSectionHeader] = React.useState("");
 
   return (<>
-      {!show && (<Card onClick={() => setShow(true)}>
+    <Card onClick={() => setShow(true)}>
           Insert section header here
-      </Card>)}
-      <br />
-      {show && (
-          <>
-              <DisplayText>Section Header</DisplayText>
-              <TextInput></TextInput>
-          </>
-        )}
+          <br />
+          {show && (
+              <TextInput 
+                value={sectionHeader} 
+                name="section-header" 
+                onChange={(e) => setSectionHeader(e.target.value)}>
+              </TextInput>
+          )}
+        </Card>
       <br/>
       <RichTextEditor sdk={sdk} isInitiallyDisabled={false} minHeight={300} maxHeight={900}>
       </RichTextEditor>
