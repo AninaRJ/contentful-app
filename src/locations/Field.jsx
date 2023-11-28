fa2b30eef33aa799cba5695cbb7d8e50f5011944
AninaRJ <ranjani.shivashankar@gmail.com>
2023-11-28 16:06:56 +0530

    Handle radio

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BooleanEditor } from '@contentful/field-editor-boolean';
+import { DisplayText } from '@contentful/f36-components';
 
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 
 const Field = () => {
   const sdk = useSDK();
 
+  const [anchorSections, setAnchorSections] = useState(false);
+
   if(sdk.field.id === 'longFormBody'){
-    return <><BooleanEditor field={sdk.field}></BooleanEditor></>;
+    return <>
+      <DisplayText>Are sections anchored?</DisplayText>
+      <BooleanEditor field={sdk.field} onValueChanged={setAnchorSections}></BooleanEditor>
+
+      {anchorSections && <>
+        <DisplayText>Yep</DisplayText>
+      </>}
+    </>;
   }
   else{
     return <></>;
