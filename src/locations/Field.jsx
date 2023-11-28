09f33fff57532a431fde7f5f3d0d3fa4f0af9c1b
AninaRJ <ranjani.shivashankar@gmail.com>
2023-11-28 19:04:27 +0530

    Modal window

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ToggleButton } from '@contentful/f36-components';
-import { Subheading } from '@contentful/f36-components';
+import { Modal } from '@contentful/f36-components';
 import { TextInput } from '@contentful/f36-components';
 
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
@@ -9,24 +9,31 @@ import { RichTextEditor } from '@contentful/field-editor-rich-text';
 const Field = () => {
   const sdk = useSDK();
   const [sectionHeader, setSectionHeader] = React.useState("");
-  const [showHeader, setShowHeader] = React.useState(false);
+  const [isShown, setShown] = React.useState(false);
 
   return (<>
-        <ToggleButton variant="primary" onClick={() => setShowHeader(!showHeader)}>Add Section Heading</ToggleButton>
+        <ToggleButton variant="primary" onClick={() => setShown(true)}>Add Section Heading</ToggleButton>
         <br/>
         {
-          showHeader && 
-          <>
-            <Subheading as="span" marginBottom='none'>
-              Section Heading
-            </Subheading>
-            <TextInput 
-              value={sectionHeader} 
-              placeholder="Insert the section header here"
-              name="section-header" 
-              onChange={(e) => setSectionHeader(e.target.value)}>
-            </TextInput>
-          </>
+          <Modal onClose={() => setShown(false)} isShown={isShown}>
+              {() => (
+                <>
+                  <Modal.Header
+                    title="Section Heading"
+                    subtitle="subtitle"
+                    onClose={() => setShown(false)}
+                  />
+                  <Modal.Content>
+                      <TextInput 
+                        value={sectionHeader} 
+                        placeholder="Insert the section header here"
+                        name="section-header" 
+                        onChange={(e) => setSectionHeader(e.target.value)}>
+                      </TextInput>
+                  </Modal.Content>
+                </>
+              )}
+            </Modal>
         }
       
       <br/><br/>
