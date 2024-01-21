53eda455e6a2e847e63053effaa8ceb17bd2b8a4
AninaRJ <ranjani.shivashankar@gmail.com>
2024-01-21 16:09:35 +0530

    Update to editor

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,26 +1,61 @@
 import React, { useState } from 'react';
 
 import {  useSDK } from '@contentful/react-apps-toolkit';
-import { FormControl, TextInput } from '@contentful/f36-components';
+import { Button, Flex, FormControl, IconButton, Stack, Textarea, Tooltip } from '@contentful/f36-components';
+import { FormatBoldIcon, FormatItalicIcon } from '@contentful/f36-icons';
+
+import { css } from 'emotion';
+import tokens from '@contentful/f36-tokens';
+
+function IconButtonToolbarExample() {
+  const styles = {
+    editorToolbarContainer: css({
+      backgroundColor: tokens.gray200,
+      borderRadius: tokens.borderRadiusMedium,
+    }),
+  };
+  return (
+    <Flex
+      justifyContent="space-between"
+      className={styles.editorToolbarContainer}
+      padding="spacingXs"
+    >
+      <Stack spacing="spacingXs">
+        <Tooltip content="Bold">
+          <IconButton
+            aria-label="Toggle bold"
+            icon={<FormatBoldIcon />}
+            variant="transparent"
+            size="small"
+          />
+        </Tooltip>
+        <Tooltip content="Italic">
+          <IconButton
+            aria-label="Toggle italic"
+            icon={<FormatItalicIcon />}
+            variant="transparent"
+            size="small"
+          />
+        </Tooltip>
+      </Stack>
+      <Button>Example button</Button>
+    </Flex>
+  );
+}
 
 const Field = () => {
   let sdk = useSDK();
 
-  const [value, setValue] = useState('your-email@domain.com');
+  const [value, setValue] = useState('');
   return (
     <FormControl isRequired isInvalid={!value} sdk={sdk}>
-      <FormControl.Label>Email</FormControl.Label>
-      <TextInput
-        value={value}
-        type="email"
-        name="email"
-        placeholder="your-email@domain.com"
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <FormControl.HelpText>Provide your email address</FormControl.HelpText>
+      <FormControl.Label>Custom Rich Text Text</FormControl.Label>
+      {IconButtonToolbarExample()}
+      <Textarea>
+      </Textarea>
       {!value && (
         <FormControl.ValidationMessage>
-          Please, provide your email
+          Please enter some text
         </FormControl.ValidationMessage>
       )}
     </FormControl>
